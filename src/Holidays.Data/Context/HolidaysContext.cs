f12cdfb868306d489c21a5cab0a022e74c3e399c
Leonardo Buta <leonardo.lbuta@gmail.com>
2021-05-11 18:28:31 -0300

    Initial structure

---
+++ src/Holidays.Data/Context/HolidaysContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Holidays.Data.Context
+{
+    public class HolidaysContext : DbContext
+    {
+        public HolidaysContext(DbContextOptions<HolidaysContext> options) : base(options) { }
+    }
+}
